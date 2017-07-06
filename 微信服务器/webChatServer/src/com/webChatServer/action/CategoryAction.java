abf9f835c8f6eea9beb415e492f9189e3b6592d3
QianYang <QianYang@244d0f91-ff8a-4924-8869-ed5808972776>
2017-07-06 05:58:14 +0000

    SSH整合全部搭建完成

--- 微信服务器/webChatServer/src/com/webChatServer/action/CategoryAction.java
+++ 微信服务器/webChatServer/src/com/webChatServer/action/CategoryAction.java
@@ -1,11 +1,14 @@
 package com.webChatServer.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.webChatServer.model.Category;
 import com.webChatServer.server.iface.CategoryService;
 
 public class CategoryAction extends ActionSupport{
 	//设置categoryService是为了很直观的看出与Spring整合前后的不同  
 	private CategoryService categoryService;
+	//设置一个私有成员变量接收url带过来的参数，注意下面要写好get和set方法  
+	private Category category;
 	
 	public void setCategoryServer(CategoryService categoryService){
 		this.categoryService = categoryService;
@@ -14,16 +17,26 @@ public class CategoryAction extends ActionSupport{
 	public String update(){
 		System.out.println("****update****");
 		//整合前后输出不同  
-		System.out.println(categoryService);
+		System.out.println(categoryService);//由于已经和Spring整合，所以可以拿到这个categoryService了，打印出来就不是null了  
+		categoryService.update(category);//新加一条语句，来更新数据库  
 		return "index";
 	}
 	
 	public String save(){
 		System.out.println("****save****");
 		//整合前后输出不同  
-		System.out.println(categoryService);
+		System.out.println(categoryService);//由于已经和Spring整合，所以可以拿到这个categoryService了，打印出来就不是null了  
+		categoryService.save(category);//新加一条语句，来更新数据库  
 		return "index";
 		
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 }
 
